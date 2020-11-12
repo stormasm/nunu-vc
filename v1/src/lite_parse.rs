66d006ed95c22659806e7b4341ac2955cb46d4c0
Michael Angerman <stormasm@gmail.com>
2020-11-12 07:59:41 -0800

    add in v1 where I will start making modifications to the code

---
+++ v1/src/lite_parse.rs
@@ -0,0 +1,151 @@
+use std::iter::Peekable;
+use std::str::CharIndices;
+
+use crate::language::{BlockKind, ParseError, Span, Spanned, SpannedItem, Token, TokenContents};
+
+pub type Input<'t> = Peekable<CharIndices<'t>>;
+
+/// Finds the extents of a bare (un-classified) token, returning the string with its associated span,
+/// along with any parse error that was discovered along the way.
+/// Bare tokens are unparsed content separated by spaces or a command separator (like pipe or semicolon)
+/// Bare tokens may be surrounded by quotes (single, double, or backtick) or braces (square, paren, curly)
+pub fn bare(src: &mut Input, span_offset: usize) -> (Spanned<String>, Option<ParseError>) {
+    let mut bare = String::new();
+    let start_offset = if let Some((pos, _)) = src.peek() {
+        *pos
+    } else {
+        0
+    };
+
+    let mut inside_quote: Option<char> = None;
+    let mut block_level: Vec<BlockKind> = vec![];
+
+    while let Some((_, c)) = src.peek() {
+        let c = *c;
+        if inside_quote.is_some() {
+            if Some(c) == inside_quote {
+                inside_quote = None;
+            }
+        } else if c == '\'' || c == '"' || c == '`' {
+            inside_quote = Some(c);
+        } else if c == '[' {
+            block_level.push(BlockKind::SquareBracket);
+        } else if c == ']' {
+            if let Some(BlockKind::SquareBracket) = block_level.last() {
+                let _ = block_level.pop();
+            }
+        } else if c == '{' {
+            block_level.push(BlockKind::CurlyBracket);
+        } else if c == '}' {
+            if let Some(BlockKind::CurlyBracket) = block_level.last() {
+                let _ = block_level.pop();
+            }
+        } else if c == '(' {
+            block_level.push(BlockKind::Paren);
+        } else if c == ')' {
+            if let Some(BlockKind::Paren) = block_level.last() {
+                let _ = block_level.pop();
+            }
+        } else if block_level.is_empty() && (c.is_whitespace() || c == '|' || c == ';') {
+            break;
+        }
+        bare.push(c);
+        let _ = src.next();
+    }
+
+    let span = Span::new(
+        start_offset + span_offset,
+        start_offset + span_offset + bare.len(),
+    );
+
+    if let Some(block) = block_level.last() {
+        let delim: char = (*block).into();
+        let cause = ParseError::UnexpectedEof(delim.to_string(), span);
+
+        while let Some(bk) = block_level.pop() {
+            bare.push(bk.into());
+        }
+
+        return (bare.spanned(span), Some(cause));
+    }
+
+    if let Some(delimiter) = inside_quote {
+        // The non-lite parse trims quotes on both sides, so we add the expected quote so that
+        // anyone wanting to consume this partial parse (e.g., completions) will be able to get
+        // correct information from the non-lite parse.
+        bare.push(delimiter);
+
+        return (
+            bare.spanned(span),
+            Some(ParseError::UnexpectedEof(delimiter.to_string(), span)),
+        );
+    }
+
+    if bare.is_empty() {
+        return (
+            bare.spanned(span),
+            Some(ParseError::UnexpectedEof("command".to_string(), span)),
+        );
+    }
+
+    (bare.spanned(span), None)
+}
+
+/// Breaks the input string into a vector of tokens. This tokenization only tries to classify separators like
+/// semicolons, pipes, etc from external bare values (values that haven't been classified further)
+/// Takes in a string and and offset, which is used to offset the spans created (for when this function is used to parse inner strings)
+pub fn lex(input: &str, span_offset: usize) -> (Vec<Token>, Option<ParseError>) {
+    let mut char_indices = input.char_indices().peekable();
+    let mut error = None;
+
+    let mut output = vec![];
+
+    while let Some((idx, c)) = char_indices.peek() {
+        if *c == '|' {
+            let idx = *idx;
+            let prev_idx = idx;
+            let _ = char_indices.next();
+            if let Some((idx, c)) = char_indices.peek() {
+                if *c == '|' {
+                    // we have '||' instead of '|'
+                    let idx = *idx;
+                    let _ = char_indices.next();
+                    output.push(Token::new(
+                        TokenContents::Bare("||".into()),
+                        Span::new(span_offset + prev_idx, span_offset + idx + 1),
+                    ));
+                    continue;
+                }
+            }
+            output.push(Token::new(
+                TokenContents::Pipe,
+                Span::new(span_offset + idx, span_offset + idx + 1),
+            ));
+        } else if *c == ';' {
+            let idx = *idx;
+            let _ = char_indices.next();
+            output.push(Token::new(
+                TokenContents::Semicolon,
+                Span::new(span_offset + idx, span_offset + idx + 1),
+            ));
+        } else if *c == '\n' || *c == '\r' {
+            let idx = *idx;
+            let _ = char_indices.next();
+            output.push(Token::new(
+                TokenContents::EOL,
+                Span::new(span_offset + idx, span_offset + idx + 1),
+            ));
+        } else if c.is_whitespace() {
+            let _ = char_indices.next();
+        } else {
+            let (result, err) = bare(&mut char_indices, span_offset);
+            if error.is_none() {
+                error = err;
+            }
+            let Spanned { item, span } = result;
+            output.push(Token::new(TokenContents::Bare(item), span));
+        }
+    }
+
+    (output, error)
+}
---
+++ v1/src/parse.rs
@@ -0,0 +1,166 @@
+use crate::language::{
+    Command, Expression, ExpressionGroup, ExpressionPipeline, ExpressionShape, Group, LiteBlock,
+    ParseError, Pipeline, Scope, Span, Spanned, SpannedExpression, SpannedItem, Token,
+    TokenContents,
+};
+use crate::lite_parse::lex;
+
+fn group(tokens: Vec<Token>) -> (LiteBlock, Option<ParseError>) {
+    let mut groups = vec![];
+    let mut group = Group::new();
+    let mut pipeline = Pipeline::new();
+    let mut command = Command::new();
+
+    for token in tokens {
+        match token.contents {
+            TokenContents::EOL => {
+                if !command.is_empty() {
+                    pipeline.push(command);
+                    command = Command::new();
+                }
+                if !pipeline.is_empty() {
+                    group.push(pipeline);
+                    pipeline = Pipeline::new();
+                }
+                if !group.is_empty() {
+                    groups.push(group);
+                    group = Group::new();
+                }
+            }
+            TokenContents::Pipe => {
+                if !command.is_empty() {
+                    pipeline.push(command);
+                    command = Command::new();
+                } else {
+                    return (
+                        LiteBlock::new(groups),
+                        Some(ParseError::UnexpectedPipe(token.span)),
+                    );
+                }
+            }
+            TokenContents::Semicolon => {
+                if !command.is_empty() {
+                    pipeline.push(command);
+                    command = Command::new();
+                }
+                if !pipeline.is_empty() {
+                    group.push(pipeline);
+                    pipeline = Pipeline::new();
+                }
+            }
+            TokenContents::Bare(bare) => {
+                command.push(bare.spanned(token.span));
+            }
+        }
+    }
+    if !command.is_empty() {
+        pipeline.push(command);
+    }
+    if !pipeline.is_empty() {
+        group.push(pipeline);
+    }
+    if !group.is_empty() {
+        groups.push(group);
+    }
+
+    (LiteBlock::new(groups), None)
+}
+
+fn parse_expr(
+    s: &Spanned<String>,
+    shape: ExpressionShape,
+) -> (SpannedExpression, Option<ParseError>) {
+    match shape {
+        ExpressionShape::Integer => {
+            if let Some(i) = num_bigint::BigInt::parse_bytes(s.item.as_bytes(), 10) {
+                (Expression::Integer(i).spanned(s.span), None)
+            } else {
+                (
+                    Expression::Garbage.spanned(s.span),
+                    Some(ParseError::UnexpectedType {
+                        expected: "Integer".into(),
+                        span: s.span,
+                    }),
+                )
+            }
+        }
+        ExpressionShape::String => {
+            // Pretty much everything else counts as some kind of string
+            (Expression::String(s.item.clone()).spanned(s.span), None)
+        }
+    }
+}
+
+fn parse_call(call: Command, _scope: &Box<Scope>) -> (SpannedExpression, Option<ParseError>) {
+    if call.elements.is_empty() {
+        (
+            Expression::Garbage.spanned_unknown(),
+            Some(ParseError::UnexpectedType {
+                expected: "call".into(),
+                span: Span::unknown(),
+            }),
+        )
+    } else {
+        let mut err = None;
+        let (head, error) = parse_expr(&call.elements[0], ExpressionShape::String);
+        let mut span = head.span;
+        if err.is_none() {
+            err = error;
+        }
+
+        let mut args = vec![];
+        for arg in call.elements.iter().skip(1) {
+            let (arg, error) = parse_expr(arg, ExpressionShape::Integer);
+            if err.is_none() {
+                err = error;
+            }
+            span.extend(arg.span);
+            args.push(arg);
+        }
+
+        (Expression::Call(Box::new(head), args).spanned(span), err)
+    }
+}
+
+fn parse_helper(
+    lite_block: LiteBlock,
+    scope: &Box<Scope>,
+) -> (Vec<ExpressionGroup>, Option<ParseError>) {
+    let mut output = vec![];
+    let mut err = None;
+
+    for group in lite_block.groups {
+        let mut out_group = ExpressionGroup::new();
+        for pipeline in group.pipelines {
+            let mut out_pipe = ExpressionPipeline::new();
+            for call in pipeline.commands {
+                let (parsed, error) = parse_call(call, scope);
+                if err.is_none() {
+                    err = error;
+                }
+                out_pipe.push(parsed);
+            }
+            out_group.push(out_pipe);
+        }
+        output.push(out_group);
+    }
+
+    (output, err)
+}
+
+pub fn parse(
+    input: &str,
+    span_offset: usize,
+    scope: &Box<Scope>,
+) -> (Vec<ExpressionGroup>, Option<ParseError>) {
+    let (output, error) = lex(input, span_offset);
+    if error.is_some() {
+        return (vec![], error);
+    }
+    let (groups, error) = group(output);
+    if error.is_some() {
+        return (vec![], error);
+    }
+
+    parse_helper(groups, scope)
+}
